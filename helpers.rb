85ceb0279ff55ed0f67fd8fc67b6b811d62d11a4
Greg Nokes <tsykoduk@gmail.com>
2015-08-25 13:56:06 -0700

    removed opty stuff for now

--- helpers.rb
+++ helpers.rb
@@ -4,12 +4,6 @@ def archive()
   a = 0
   sfdc_accounts = SfdcAccounts.all
   total_a = sfdc_accounts.count
-
-#  Currently, the only scope is accounts. This is for future stuff
-#  o = 0
-#  sfdc_optys = SfdcOpportunitys.all
-#  total_o = sfdc_optys.count
-  
   sfdc_accounts.each do |sfdc_account|
     account = Accounts.new(sfdc_account.attributes)
     account.save
@@ -17,15 +11,6 @@ def archive()
     puts "moved account #" + a.to_s + " of " + total_a.to_s
     a = a + 1
   end
-  
- # sfdc_optys.each do |sfdc_opty|
- #   opty = Opportunitys.new(sfdc_opty.attributes)
- #   opty.save
- #   #sfdc_opty.destroy
- #   puts "moved opts #" + o.to_s + " of " + total_o.to_s
- #   o = o + 1
- # end
-  
 end
 
 
@@ -37,7 +22,6 @@ def put_into_sfdc()
   total_a = accounts
   optys = Opportunitys.all
   total_o = optys
-  
   accounts.each do |account|
     sfdc_account = SfdcAccounts.new(account.attributes)
     sfdc_account.save
